27ed49e3e7b5ba29ccd7f30c21f3768ff86ac695
Venkat <venkat@tillidsoft.com>
2013-02-21 12:17:25 +0530

    commit 2

--- Paypaltillidtest/Thanks.aspx
+++ Paypaltillidtest/Thanks.aspx
@@ -1,1 +1,5 @@
 ﻿<%@ Page Title="" Language="C#"  AutoEventWireup="true" CodeBehind="Thanks.aspx.cs" Inherits="Paypaltillidtest.Thanks" %>
+<form id="form1" runat="server">
+<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+<asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+</form>
--- Paypaltillidtest/Thanks.aspx.cs
+++ Paypaltillidtest/Thanks.aspx.cs
@@ -15,56 +15,54 @@ namespace Paypaltillidtest
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string postUrl = "https://sandbox.paypal.com/cgi-bin/webscr";//ConfigurationManager.AppSettings["PayPalSubmitUrl"];
-       
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(postUrl);
-
-            //Set values for the request back
-            req.Method = "POST";
-            req.ContentType = "application/x-www-form-urlencoded";
-            byte[] param = Request.BinaryRead(HttpContext.Current.Request.ContentLength);
-            string strRequest = Encoding.ASCII.GetString(param);
-            string ipnPost = strRequest;
-            strRequest += "&cmd=_notify-validate";
-            req.ContentLength = strRequest.Length;
-
-            //for proxy
-            //WebProxy proxy = new WebProxy(new Uri("http://url:port#"));
-            //req.Proxy = proxy;
-
-            //Send the request to PayPal and get the response
-            StreamWriter streamOut = new StreamWriter(req.GetRequestStream(),
-                                     System.Text.Encoding.ASCII);
-            streamOut.Write(strRequest);
-            streamOut.Close();
-
-            StreamReader streamIn = new StreamReader(req.GetResponse().GetResponseStream());
-            string strResponse = streamIn.ReadToEnd();
-            streamIn.Close();
-
-            // logging ipn messages... be sure that you give write
-            // permission to process executing this code
-            string logPathDir = ResolveUrl("Messages");
-            string logPath = string.Format("{0}\\{1}.txt",
-                             Server.MapPath(logPathDir), DateTime.Now.Ticks);
-            File.WriteAllText(logPath, ipnPost);
-            //
-
-            if (strResponse == "VERIFIED")
+            if (!Page.IsPostBack)
             {
-                //check the payment_status is Completed
-                //check that txn_id has not been previously processed
-                //check that receiver_email is your Primary PayPal email
-                //check that payment_amount/payment_currency are correct
-                //process payment
-            }
-            else if (strResponse == "INVALID")
-            {
-                //log for manual investigation
-            }
-            else
-            {
-                //log response/ipn data for manual investigation
+                string authToken = "uK - wFGY8qA17liEbyJqqvlFZRHuL7E3JMib0ABdhyj0o2B8Szu3LpIO80b0";
+
+                //read in txn token from querystring
+                string txToken = Request.QueryString.Get("tx");
+
+
+                string query = string.Format("cmd=_notify-synch&tx={0}&at={1}",
+                                      txToken, authToken);
+
+                // Create the request back
+                string url = "https://sandbox.paypal.com/cgi-bin/webscr";
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+                // Set values for the request back
+                req.Method = "POST";
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.ContentLength = query.Length;
+
+                // Write the request back IPN strings
+                StreamWriter stOut = new StreamWriter(req.GetRequestStream(),
+                                         System.Text.Encoding.ASCII);
+                stOut.Write(query);
+                stOut.Close();
+
+                // Do the request to PayPal and get the response
+                StreamReader stIn = new StreamReader(req.GetResponse().GetResponseStream());
+                string strResponse = stIn.ReadToEnd();
+                stIn.Close();
+
+                // sanity check
+                Label2.Text = strResponse;
+
+                // If response was SUCCESS, parse response string and output details
+                if (strResponse.StartsWith("SUCCESS"))
+                {
+                   /* PDTHolder pdt = PDTHolder.Parse(strResponse);
+                    Label1.Text =
+                        string.Format("Thank you {0} {1} [{2}] for your payment of {3} {4}!",
+                        pdt.PayerFirstName, pdt.PayerLastName,
+                        pdt.PayerEmail, pdt.GrossTotal, pdt.Currency);*/
+                    Label1.Text = strResponse;
+                }
+                else
+                {
+                    Label1.Text = "Oooops, something went wrong...";
+                }
             }
         }
     }
