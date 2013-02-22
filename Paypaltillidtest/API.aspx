7e88ad8f075ede6ac55df3199a2f3a0e4fed9106
Venkat <venkat@tillidsoft.com>
2013-02-22 17:32:32 +0530

    api1

---
+++ Paypaltillidtest/API.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="API.aspx.cs" Inherits="Paypaltillidtest.API" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <form id="form2" runat="server">
+    <div>
+    
+        <br />
+        Start Express Checkout Process:<br />
+        <br />
+        <asp:Button ID="oneTimeButton" runat="server" Text="One Time Payment" 
+            Width="200px" onclick="oneTimeButton_Click"  />
+    </div>
+    
+    </form>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Paypaltillidtest/API.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Paypaltillidtest.com.paypal.sandbox.www;
+
+namespace Paypaltillidtest
+{
+    public partial class API : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void oneTimeButton_Click(object sender, EventArgs e)
+        {// build request
+            SetExpressCheckoutRequestDetailsType reqDetails = new SetExpressCheckoutRequestDetailsType();
+
+            // TODO: Be sure to update hosting address in Web.Config!!
+            string hostingOn = ConfigurationManager.AppSettings["HostingPrefix"];
+
+            reqDetails.ReturnURL = hostingOn + "/Success.aspx";
+            reqDetails.CancelURL = hostingOn + "/Cancel.aspx";
+            reqDetails.NoShipping = "1";
+            reqDetails.OrderTotal = new BasicAmountType()
+            {
+                currencyID = CurrencyCodeType.USD,
+                Value = "10.00"
+            };
+
+            SetExpressCheckoutReq req = new SetExpressCheckoutReq()
+            {
+                SetExpressCheckoutRequest = new SetExpressCheckoutRequestType()
+                {
+                    Version = UtilPayPalAPI.Version,
+                    SetExpressCheckoutRequestDetails = reqDetails
+                }
+            };
+
+            // query PayPal and get token
+            SetExpressCheckoutResponseType resp = UtilPayPalAPI.BuildPayPalWebservice().SetExpressCheckout(req);
+            UtilPayPalAPI.HandleError(resp);
+
+            // redirect user to PayPal
+            Response.Redirect(string.Format("{0}?cmd=_express-checkout&token={1}",
+                ConfigurationManager.AppSettings["PayPalSubmitUrl"], resp.Token));
+
+        }
+    }
+    public class UtilPayPalAPI
+    {
+        public static string Version
+        {
+            get { return ConfigurationManager.AppSettings["APIVersion"]; }
+        }
+
+        public static PayPalAPIAASoapBinding BuildPayPalWebservice()
+        {
+            // more details on https://www.paypal.com/en_US/ebook/PP_APIReference/architecture.html
+            UserIdPasswordType credentials = new UserIdPasswordType()
+            {
+                Username = ConfigurationManager.AppSettings["APIUsername"],
+                Password = ConfigurationManager.AppSettings["APIPassword"],
+                Signature = ConfigurationManager.AppSettings["APISignature"],
+            };
+
+            PayPalAPIAASoapBinding paypal = new PayPalAPIAASoapBinding();
+            paypal.RequesterCredentials = new CustomSecurityHeaderType()
+            {
+                Credentials = credentials
+            };
+
+            return paypal;
+        }
+
+        internal static void HandleError(AbstractResponseType resp)
+        {
+            if (resp.Errors != null && resp.Errors.Length > 0)
+            {
+                // errors occured
+                throw new Exception("Exception(s) occured when calling PayPal. First exception: " +
+                    resp.Errors[0].LongMessage);
+            }
+        }
+    }
+}
